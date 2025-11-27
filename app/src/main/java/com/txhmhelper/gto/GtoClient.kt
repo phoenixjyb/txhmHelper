9e5e559824c15b6dfb7102b83985e0f2cff4fbe4
yanbo <yanbophx@hotmail.com>
2025-11-27 14:02:34 +0800

    Force GTO client to bypass system HTTP proxy

--- app/src/main/java/com/txhmhelper/gto/GtoClient.kt
+++ app/src/main/java/com/txhmhelper/gto/GtoClient.kt
@@ -3,6 +3,7 @@ package com.txhmhelper.gto
 import com.squareup.moshi.Moshi
 import com.txhmhelper.BuildConfig
 import okhttp3.OkHttpClient
+import java.net.Proxy
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
@@ -11,6 +12,8 @@ object GtoClient {
     private val moshi: Moshi = Moshi.Builder().build()
 
     private val okHttp = OkHttpClient.Builder()
+        // Avoid system HTTP proxies that might block local network calls.
+        .proxy(Proxy.NO_PROXY)
         .connectTimeout(5, TimeUnit.SECONDS)
         .readTimeout(5, TimeUnit.SECONDS)
         .build()
